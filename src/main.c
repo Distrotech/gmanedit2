0f3d5973b057098b022dd0c25320f76be7b35da0
Joop Stakenborg <pg4i@amsat.org>
2008-02-27 14:04:53 +0000

    cleanups and license added

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 /*
  *  Copyright 2000-2001 Sergio Rua <srua@debian.org>
+ *  Copyright 2008 Joop Stakenborg <pg4i@amsat.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,10 +16,6 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
 
 #include <gnome.h>
 #include <gtk/gtk.h>
