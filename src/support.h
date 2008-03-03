6e2b776310d8055bd3c418ea843294e69a8eae4a
Joop Stakenborg <pg4i@amsat.org>
2008-03-03 13:14:19 +0000

    fix some NLS issues

--- src/support.h
+++ src/support.h
@@ -17,15 +17,23 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include <libintl.h>
-
-#undef _
-#define _(String) dgettext (PACKAGE, String)
-
-#ifdef gettext_noop
+#ifdef ENABLE_NLS
+  #include <libintl.h>
+  #undef _
+  #define _(String) dgettext (PACKAGE, String)
+  #ifdef gettext_noop
 	#define N_(String) gettext_noop (String)
-#else
+  #else
 	#define N_(String) (String)
+  #endif
+#else
+  #define textdomain(String) (String)
+  #define gettext(String) (String)
+  #define dgettext(Domain,Message) (Message)
+  #define dcgettext(Domain,Message,Type) (Message)
+  #define bindtextdomain(Domain,Directory) (Domain)
+  #define _(String) (String)
+  #define N_(String) (String)
 #endif
 
 GtkWidget*  lookup_widget              (GtkWidget       *widget,
